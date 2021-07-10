88a8d8ebf39c2ca9b19c036e30abd62e799993b4
lufuhu <760404838@qq.com>
2021-07-10 11:54:04 +0800

    1

--- app/Models/WorldCity.php
+++ app/Models/WorldCity.php
@@ -27,18 +27,18 @@ class WorldCity extends BaseModel
         $data = self::where('code', $code)->get();
         foreach ($data as $k=>$v){
             $list[$k] = ["value" => $v->name_zh,"label"=> $v->name_zh];
-            $children = self::where('pid', $v->id)->get();
-            if ($children){
-                foreach ($children as $k1=>$v1){
-                    $list[$k]['children'][$k1] = ["value" => $v1->name_zh,"label"=> $v1->name_zh];
-                    $children2 = self::where('pid', $v1->id)->get();
-                    if ($children2){
-                        foreach ($children2 as $k2=>$v2){
-                            $list[$k]['children'][$k1]['children'][$k2] = ["value" => $v2->name_zh,"label"=> $v2->name_zh];
-                        }
-                    }
-                }
-            }
+//            $children = self::where('pid', $v->id)->get();
+//            if ($children){
+//                foreach ($children as $k1=>$v1){
+//                    $list[$k]['children'][$k1] = ["value" => $v1->name_zh,"label"=> $v1->name_zh];
+//                    $children2 = self::where('pid', $v1->id)->get();
+//                    if ($children2){
+//                        foreach ($children2 as $k2=>$v2){
+//                            $list[$k]['children'][$k1]['children'][$k2] = ["value" => $v2->name_zh,"label"=> $v2->name_zh];
+//                        }
+//                    }
+//                }
+//            }
         }
         return $list;
     }
