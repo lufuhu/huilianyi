ef5f2a5777773db15daef3afefa285706f7985f5
lufuhu <760404838@qq.com>
2021-07-10 12:11:47 +0800

    1

--- app/Models/WorldCity.php
+++ app/Models/WorldCity.php
@@ -13,31 +13,42 @@ class WorldCity extends BaseModel
         "code_all",
     ];
 
-    public static function getAbroadCountry(){
+    public static function getAbroadCountry()
+    {
         $data = self::where('pid', 0)->where('id', '>', 1)->get();
         $list = [];
-        foreach ($data as $k=>$v) {
+        foreach ($data as $k => $v) {
             $list[] = ["value" => $v->id, "label" => $v->name_zh];
         }
         return $list;
     }
 
-    public static function getCityList($pid){
+    public static function getCityList($pid)
+    {
         $list = [];
         $data = self::where('pid', $pid)->get();
-        foreach ($data as $k=>$v){
-            $list[$k] = ["value" => $v->id,"label"=> $v->name_zh];
+        foreach ($data as $k => $v) {
+            $list[$k] = ["value" => $v->id, "label" => $v->name_zh];
             $children = self::where('pid', $v->id)->get();
-            if ($children){
-                foreach ($children as $k1=>$v1){
-                    $list[$k]['children'][$k1] = ["value" => $v1->id,"label"=> $v1->name_zh];
+            if ($children) {
+                foreach ($children as $k1 => $v1) {
+                    $list[$k]['children'][$k1] = ["value" => $v1->id, "label" => $v1->name_zh];
                     $children2 = self::where('pid', $v1->id)->get();
-                    if ($children2){
-                        foreach ($children2 as $k2=>$v2){
-                            $list[$k]['children'][$k1]['children'][$k2] = ["value" => $v2->id,"label"=> $v2->name_zh];
+                    if ($children2) {
+                        foreach ($children2 as $k2 => $v2) {
+                            $list[$k]['children'][$k1]['children'][$k2] = ["value" => $v2->id, "label" => $v2->name_zh];
                         }
+                    } else {
+                        $list[$k]['children'][$k1]['children'] = [["value" => $v1->id, "label" => $v1->name_zh]];
                     }
                 }
+            } else {
+                $list[$k]['children'] = [[
+                    "value" => $v->id,
+                    "label" => $v->name_zh,
+                    'children' =>
+                        [["value" => $v->id, "label" => $v->name_zh]]
+                ]];
             }
         }
         return $list;
